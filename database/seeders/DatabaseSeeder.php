bf7f0bfc0b799762d6bfe1dd1a9855dbb5929ba9
Fouad <fouadnassif2005@gmail.com>
2025-03-23 16:44:51 +0200

    Test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Item;
 use App\Models\ItemCategory;
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
